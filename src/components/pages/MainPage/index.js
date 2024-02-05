56fca2dacfd03c761e6374c9fe8f11c1f6c2b85b
LamiDs <c_rux@naver.com>
2024-02-05 11:09:40 +0900

    Row.js 껍데기 제작

--- src/components/pages/MainPage/index.js
+++ src/components/pages/MainPage/index.js
@@ -1,7 +1,42 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import { getFestival } from '../../../api/testapi/get/getFestival';
+import Banner from '../../Banner';
+import Header from '../../Header';
+import Row from '../../Row';
 
 const MainPage = () => {
-  return <div>MainPage</div>;
+  //mockData 받아오기 추후 각 컴포넌트 별로 api 받기
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    const fetchMockData = async () => {
+      try {
+        const response = await getFestival();
+        setData(response);
+      } catch (err) {
+        console.error(err);
+      }
+    };
+    fetchMockData();
+  }, []);
+  return (
+    <Container className="App">
+      <Header />
+      <Banner />
+      <Row title="관광지" id="RM" data={data} />
+      <Row title="팝업스토어" id="PS" data={data} />
+      <Row title="플리마켓" id="FM" data={data} />
+      <Row title="페스티벌" id="FV" data={data} />
+    </Container>
+  );
 };
 
 export default MainPage;
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  display: block;
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
+`;
