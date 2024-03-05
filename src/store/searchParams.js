4f6ca8ee15c009cbd8f4e0d4c1119a1176706b00
LamiDs <c_rux@naver.com>
2024-03-05 18:53:04 +0900

    redux 오류 수정

--- src/store/searchParams.js
+++ src/store/searchParams.js
@@ -1,20 +1,28 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-export const searchParams = createSlice({
+const initialState = {
+  params: {
+    keyword: '',
+    cat1: '',
+  },
+};
+
+// setKeyword reducer
+const setKeywordReducer = (state, action) => {
+  state.params.keyword = action.payload;
+};
+
+// setCat1 reducer
+const setCat1Reducer = (state, action) => {
+  state.params.cat1 = action.payload;
+};
+
+const searchParams = createSlice({
   name: 'searchParams',
-  initialState: {
-    params: {
-      keyword: '',
-      cat1: '',
-    },
-    reducers: {
-      setKeyword: (state, action) => {
-        state.params.keyword = action.payload;
-      },
-      setCat1: (state, action) => {
-        state.params.cat1 = action.payload;
-      },
-    },
+  initialState,
+  reducers: {
+    setKeyword: setKeywordReducer,
+    setCat1: setCat1Reducer,
   },
 });
 
